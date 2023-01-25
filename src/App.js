9b84cd7bb3b9fb90dbd1b8f89e6675ab90904914
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-01-25 17:24:44 -0600

    gave each section it's own div

--- src/App.js
+++ src/App.js
@@ -17,24 +17,32 @@ const App = () => {
         </div>
 
         {/* REVIEWS  */}
-        <h3>Reviews</h3>
-        <h2>1,281</h2>
+        <div>
+          <h3>Reviews</h3>
+          <h2>1,281</h2>
+        </div>
 
         {/* AVERAGE RATING  */}
-        <h3>Average Rating</h3>
-        <h2>4.6</h2>
+        <div>
+          <h3>Average Rating</h3>
+          <h2>4.6</h2>
+        </div>
 
         {/* SENTIMENT ANALYSIS  */}
-        <h3>Sentiment Analysis</h3>
-        <ul>
-          <l1>960</l1>
-          <l1>122</l1>
-          <l1>321</l1>
-        </ul>
+        <div>
+          <h3>Sentiment Analysis</h3>
+          <ul>
+            <li>960</li>
+            <li>122</li>
+            <li>321</li>
+          </ul>
+        </div>
 
         {/* WEBSITE VISITORS  */}
-        <h3>Website Visitors</h3>
-        <h2>821</h2>
+        <div>
+          <h3>Website Visitors</h3>
+          <h2>821</h2>
+        </div>
 
       </div>
     </>
